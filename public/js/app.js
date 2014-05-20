ec7a0855fb237f80d5a88c410cc70aac40ed6831
Dave O <davidobrite@gmail.com>
2014-05-19 20:58:02 -0700

    Yo!

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var FluxChat = require('./components/FluxChat.react');
+
+React.renderComponent(
+  <FluxChat />,
+  document.getElementById('fluxchat')
+);
---
+++ public/js/components/FluxChat.react.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var FluxChat = React.createClass({
+  /**
+   * @return {object}
+   */
+  render: function () {
+    return (
+      <div>Yo!</div>
+    )
+  }
+});
+
+module.exports = FluxChat;
